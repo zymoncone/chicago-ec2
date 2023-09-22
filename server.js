e088abf6ef4d046d516264d352280b32b0888f03
Szymon <szymon@sarnowicz.net>
2023-09-21 21:05:10 -0500

    add node modules

--- server.js
+++ server.js
@@ -1,10 +1,44 @@
-const PORT = 8000
+// const PORT = 8000
 const express = require('express')
 const cors = require('cors')
 require('dotenv').config()
 const app = express()
 app.use(express.json())
 app.use(cors())
+
+var http = require('http');
+var debug = require('debug')('feelinghungryserver:server')
+
+/**
+ * Get port from environment and store in Express.
+*/
+
+/**
+ * Normalize a port into a number, string, or false.
+ */
+function normalizePort(val) {
+    var port = parseInt(val, 10);
+  
+    if (isNaN(port)) {
+      // named pipe
+      return val;
+    }
+  
+    if (port >= 0) {
+      // port number
+      return port;
+    }
+  
+    return false;
+  }
+
+var port = normalizePort(process.env.PORT || '8000');
+app.set('port', port);
+
+/**
+ * Create HTTP server.
+ */
+var server = http.createServer(app);
 
 // define constants
 const OPENAI_API_KEY = process.env.API_KEY
@@ -50,7 +84,49 @@ app.post('/passwords', (req, res) => {
 })
 
 app.get('/', (req, res) => {
-    res.send('Your server is running!')
+    res.send('Your server is running on Port: ' + port)
   })
 
-app.listen(PORT, () => console.log('Your server is running on Port ' + PORT))
+/**
+ * Event listener for HTTP server "error" event.
+ */
+
+function onError(error) {
+    if (error.syscall !== 'listen') {
+      throw error;
+    }
+  
+    var bind = typeof port === 'string'
+      ? 'Pipe ' + port
+      : 'Port ' + port;
+  
+    // handle specific listen errors with friendly messages
+    switch (error.code) {
+      case 'EACCES':
+        console.error(bind + ' requires elevated privileges');
+        process.exit(1);
+        break;
+      case 'EADDRINUSE':
+        console.error(bind + ' is already in use');
+        process.exit(1);
+        break;
+      default:
+        throw error;
+    }
+  }
+  
+  /**
+   * Event listener for HTTP server "listening" event.
+   */
+  
+  function onListening() {
+    var addr = server.address();
+    var bind = typeof addr === 'string'
+      ? 'pipe ' + addr
+      : 'port ' + addr.port;
+    debug('Listening on ' + bind);
+  }
+
+server.listen(port, () => console.log('Your server is running on Port ' + port))
+server.on('error', onError)
+server.on('listening', onListening)
