ccc6d38a705bc5ac8b9d02269690b694fbd3f00a
Szymon <szymon@sarnowicz.net>
2023-10-12 00:07:09 -0500

    add aws sdk features

--- server.js
+++ server.js
@@ -6,11 +6,42 @@ app.use(express.json())
 app.use(cors())
 var http = require('http')
 var debug = require('debug')('chicagoec2:server')
+const AWS = require('aws-sdk')
+
+var API_KEY
+
+const setAPIKEY = (val) => {
+  console.log('API key has been set')
+  API_KEY = val
+}
+
+// more on setting credentials here:
+// https://docs.aws.amazon.com/sdk-for-javascript/v2/developer-guide/loading-node-credentials-shared.html
+var ssm = new AWS.SSM({
+  region: 'us-east-2',
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+})
+
+var params = {
+  Name: 'API_KEY', /* required */
+  WithDecryption: true
+}
+
+ssm.getParameter(params, (err, data) => {
+  if (err) {
+    console.log(err, err.stack) // an error occurred
+  } else {
+    // console.log(response) // successful response
+    console.log("AWS SSM parameter retrieved")
+    setAPIKEY(data.Parameter.Value)
+  }    
+})
 
 /**
  * Normalize a port into a number, string, or false.
  */
-function normalizePort(val) {
+const normalizePort = (val) => {
     var port = parseInt(val, 10)
   
     if (isNaN(port)) {
@@ -30,7 +61,7 @@ function normalizePort(val) {
  * Event listener for HTTP server "error" event.
  */
 
-function onError(error) {
+const onError = (error) => {
     if (error.syscall !== 'listen') {
       throw error
     }
@@ -58,7 +89,7 @@ function onError(error) {
    * Event listener for HTTP server "listening" event.
    */
   
-  function onListening() {
+  const onListening = () => {
     var addr = server.address();
     var bind = typeof addr === 'string'
       ? 'pipe ' + addr
@@ -78,7 +109,6 @@ app.set('port', port)
 var server = http.createServer(app)
 
 // define constants
-const OPENAI_API_KEY = process.env.API_KEY
 const num_of_answers = 3
 
 // POST request to OpenAI
@@ -86,7 +116,7 @@ app.post('/completitions', async (req, res) => {
     const options = {
         method: 'POST',
         headers: {
-            'Authorization': `Bearer ${OPENAI_API_KEY}`,
+            'Authorization': `Bearer ${API_KEY}`,
             'Content-Type': 'application/json'
         },
         body: JSON.stringify({
@@ -96,7 +126,7 @@ app.post('/completitions', async (req, res) => {
         })
     }
     try {
-        res.end(`Here it is: ${OPENAI_API_KEY}`)
+        console.log('We got the key! ' + API_KEY)
         const response = await fetch('https://api.openai.com/v1/chat/completions', options)
         const data = await response.json()
         console.log(data)
